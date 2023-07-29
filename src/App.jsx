e51c773059b8b18835ce42f19a65ec4ecfcbecc4
Angel <angel91.addcs@gmail.com>
2023-07-28 20:07:00 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import './styles/testimonios.css'
 import './styles/cards.css'
 
--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,22 +1,45 @@
+import React, {useState} from 'react';
 var count = 0
 
 export default function(props) {
     count = count + 0.5
+
+    const current = count
+    var [deg, setDeg] = useState(180)
+    const spin = function(event) {
+        setDeg(deg + 180)
+        console.log(deg)
+        var card = document.getElementById(current)
+
+        var front = card.querySelector(".face.front")
+        var back = card.querySelector(".face.back")
+
+        front.style.transform = "perspective(600px) rotateY("+deg+"deg)"
+        back.style.transform = "perspective(600px) rotateY("+ (deg-180)+"deg)"
+
+        //card.classList.toggle("flip")
+    }
+
     return (
-        <div className="card">
-            <div className="image">
-                <img src={"src/assets/" + props.src} alt="IMAGE" />
+        <div className="card" id={count}  onClick={spin}>
+            <div className="face front">
+                <div className="image">
+                    <img src={"src/assets/" + props.src} alt="IMAGE" />
+                </div>
+                <div className="info">
+                    <div className="top">
+                        <p>{props.name}</p>
+                        <span>{count}</span>
+                    </div>
+                    <div className="data">
+                        <p>Strength: {props.strength}</p>
+                        <p>Speed: {props.speed}</p>
+                        <p>"{props.description}"</p>
+                    </div>
+                </div>
             </div>
-            <div className="info">
-                <div className="top">
-                    <p>{props.name}</p>
-                    <span>{count}</span>
-                </div>
-                <div className="data">
-                    <p>Strength: {props.strength}</p>
-                    <p>Speed: {props.speed}</p>
-                    <p>"{props.description}"</p>
-                </div>
+            <div className="face back">
+                <p>Lorem ipsum dolor sit amet</p>
             </div>
         </div>
     )
